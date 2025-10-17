2431d8747c7b899896537c63ad6ab389dfb7b61f
RazFernandez <mr.fernandez.mx@outlook.com>
2025-10-16 20:22:09 -0600

    Initial setup of the springboot project for meli solution

---
+++ meli_app/src/main/java/com/meli/meli_app/MeliAppApplication.java
@@ -0,0 +1,13 @@
+package com.meli.meli_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliAppApplication.class, args);
+	}
+
+}
---
+++ meli_app/src/test/java/com/meli/meli_app/MeliAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.meli_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeliAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
